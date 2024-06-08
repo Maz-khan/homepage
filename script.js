bbb3d24776790c0f67fb16181d7217cfe31f4bff
Sahil Khan <mazorkhan2000@gmail.com>
2024-06-08 22:29:15 +0530

    Home page designed by maz

---
+++ script.js
@@ -0,0 +1,21 @@
+//function myFunction(a,b){
+ //   return a*b;
+//}
+
+//console.log(myFunction(3,4));
+
+// let x=myFunction(4,3);
+
+// function myFunction(a,b){
+//     return a*b;
+// }
+
+// console.log(x);
+
+
+function toCelsius(fahrenheit){
+    return (5/9)*(fahrenheit-32);
+}
+
+console.log(toCelsius(98.2));
+
